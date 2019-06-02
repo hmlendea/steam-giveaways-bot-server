83298eab6daa722749e8823dbcd2a33ccf769cd3
Horațiu Mlendea <Mlendea.Horatiu@GMail.com>
2019-06-02 19:49:00 +0300

    Mail notifications

---
+++ Core/Configuration/MailSettings.cs
@@ -0,0 +1,11 @@
+namespace SteamGiveawaysBot.Server.Core.Configuration
+{
+    public sealed class MailSettings
+    {
+        public string RecipientAddress { get; set; }
+
+        public string SenderAddress { get; set; }
+
+        public string SenderPassword { get; set; }
+    }
+}
