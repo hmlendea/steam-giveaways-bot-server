d2fc2d42643bd24ee82988a3258ae593ef599293
Mlendea, Horațiu <Mlendea.Horatiu@GMail.com>
2019-07-17 12:22:50 +0300

    Include a Steam activation link in the email

--- Service/Models/Reward.cs
+++ Service/Models/Reward.cs
@@ -1,9 +1,14 @@
+using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 
 namespace SteamGiveawaysBot.Server.Service.Models
 {
     public sealed class Reward
     {
+        const string SteamKeyRegexPattern = "^([A-Z0-9]{5}-)*[A-Z0-9]{5}$";
+
+        const string SteamActivationUrlFormat = "https://store.steampowered.com/account/registerkey?key={0}";
+
         public string Id { get; set; }
         
         public string GiveawaysProvider { get; set; }
@@ -18,5 +23,25 @@ namespace SteamGiveawaysBot.Server.Service.Models
         public string SteamAppUrl => $"https://store.steampowered.com/app/{SteamAppId}";
 
         public string ActivationKey { get; set; }
+
+        public bool IsKeySteamCode => Regex.IsMatch(ActivationKey, SteamKeyRegexPattern);
+
+        public string ActivationLink
+        {
+            get
+            {
+                if (IsKeySteamCode)
+                {
+                    return string.Format(SteamActivationUrlFormat, ActivationKey);
+                }
+                
+                if (ActivationKey.Contains("http"))
+                {
+                    return ActivationKey;
+                }
+
+                return "UNKNOWN";
+            }
+        }
     }
 }
