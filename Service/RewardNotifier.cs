58951c5a6a1913946461b827272330b3d4085929
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-04-27 21:08:35 +0300

    Removed mail notification senders

--- Service/RewardNotifier.cs
+++ Service/RewardNotifier.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading.Tasks;
 
 using SteamGiveawaysBot.Server.Communication;
-using SteamGiveawaysBot.Server.Configuration;
 using SteamGiveawaysBot.Server.Service.Models;
 
 namespace SteamGiveawaysBot.Server.Service
@@ -10,14 +9,10 @@ namespace SteamGiveawaysBot.Server.Service
     public sealed class RewardNotifier : IRewardNotifier
     {
         readonly INotificationSender notificationSender;
-        readonly MailSettings mailSettings;
 
-        public RewardNotifier(
-            INotificationSender notificationSender,
-            MailSettings mailSettings)
+        public RewardNotifier(INotificationSender notificationSender)
         {
             this.notificationSender = notificationSender;
-            this.mailSettings = mailSettings;
         }
 
         public async Task SendNotificationAsync(Reward reward)
