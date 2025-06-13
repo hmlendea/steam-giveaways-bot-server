b6f128d6cfe9dd2d56f09e5dbd4de0835190377b
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2025-06-13 17:56:19 +0300

    error-handling: Handle missing store files

--- Startup.cs
+++ Startup.cs
@@ -1,8 +1,11 @@
+using System.IO;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SteamGiveawaysBot.Server.Configuration;
+using SteamGiveawaysBot.Server.DataAccess.DataObjects;
 
 namespace SteamGiveawaysBot.Server
 {
@@ -21,6 +24,12 @@ namespace SteamGiveawaysBot.Server
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Ensure the stores exist
+            var dataStoreSettings = app.ApplicationServices.GetRequiredService<DataStoreSettings>();
+            CreateStoreIfMissing(dataStoreSettings.RewardsStorePath, nameof(RewardEntity));
+            CreateStoreIfMissing(dataStoreSettings.UserStorePath, nameof(UserEntity));
+            CreateStoreIfMissing(dataStoreSettings.SteamAccountStorePath, nameof(SteamAccountEntity));
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -36,6 +45,23 @@ namespace SteamGiveawaysBot.Server
             {
                 endpoints.MapControllers();
             });
+        }
+
+        static void CreateStoreIfMissing(string storePath, string entityTypeName)
+        {
+            var directory = Path.GetDirectoryName(storePath);
+
+            if (!string.IsNullOrWhiteSpace(directory) &&
+                !Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+
+            if (!string.IsNullOrWhiteSpace(storePath) &&
+                !File.Exists(storePath))
+            {
+                File.WriteAllText(storePath, $"<?xml version=\"1.0\" encoding=\"utf-8\"?><{entityTypeName} xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\"></{entityTypeName}>");
+            }
         }
     }
 }
