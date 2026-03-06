e15384125791dc7cfe5703e48063835082e913e9
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-03-06 08:44:14 +0200

    datetime: Removed unused class

--- Configuration/TelegramSettings.cs
+++
@@ -1,9 +0,0 @@
-namespace SteamGiveawaysBot.Server.Configuration
-{
-    public sealed class TelegramSettings
-    {
-        public string AccessToken { get; set; }
-
-        public string ChatId { get; set; }
-    }
-}
