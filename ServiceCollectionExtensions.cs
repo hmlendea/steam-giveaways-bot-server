354e0939fbf9ed60efe469fe8ef287523f80862e
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2026-04-19 23:31:43 +0300

    refactoring: Improved config binding

--- ServiceCollectionExtensions.cs
+++ ServiceCollectionExtensions.cs
@@ -2,7 +2,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NuciDAL.Repositories;
 using NuciLog;
-using NuciLog.Configuration;
 using NuciLog.Core;
 using SteamGiveawaysBot.Server.Configuration;
 using SteamGiveawaysBot.Server.Client;
@@ -15,42 +14,31 @@ namespace SteamGiveawaysBot.Server
 {
     public static class ServiceCollectionExtensions
     {
-        static DataStoreSettings dataStoreSettings;
-        static NotificationSettings notificationSettings;
-        static SecuritySettings securitySettings;
-        static NuciNotificationsSettings nuciNotificationsSettings;
-        static NuciLoggerSettings loggingSettings;
-
         public static IServiceCollection AddConfigurations(this IServiceCollection services, IConfiguration configuration)
         {
-            dataStoreSettings = new DataStoreSettings();
-            notificationSettings = new NotificationSettings();
-            securitySettings = new SecuritySettings();
-            nuciNotificationsSettings = new NuciNotificationsSettings();
-            loggingSettings = new NuciLoggerSettings();
+            DataStoreSettings dataStoreSettings = new();
+            NotificationSettings notificationSettings = new();
+            SecuritySettings securitySettings = new();
 
             configuration.Bind(nameof(DataStoreSettings), dataStoreSettings);
             configuration.Bind(nameof(NotificationSettings), notificationSettings);
             configuration.Bind(nameof(SecuritySettings), securitySettings);
-            configuration.Bind(nameof(NuciNotificationsSettings), nuciNotificationsSettings);
-            configuration.Bind(nameof(NuciLoggerSettings), loggingSettings);
 
-            services.AddSingleton(dataStoreSettings);
-            services.AddSingleton(notificationSettings);
-            services.AddSingleton(securitySettings);
-            services.AddSingleton(nuciNotificationsSettings);
-            services.AddSingleton(loggingSettings);
-
-            return services;
+            return services
+                .AddSingleton(dataStoreSettings)
+                .AddSingleton(notificationSettings)
+                .AddSingleton(securitySettings)
+                .AddNuciNotificationsSettings(configuration)
+                .AddNuciLoggerSettings(configuration);
         }
 
         public static IServiceCollection AddCustomServices(this IServiceCollection services) => services
             .AddSingleton<IStorefrontDataRetriever, StorefrontDataRetriever>()
-            .AddSingleton<INuciNotificationsClient>(x => new NuciNotificationsClient(nuciNotificationsSettings))
+            .AddSingleton<INuciNotificationsClient>(x => new NuciNotificationsClient(x.GetRequiredService<NuciNotificationsSettings>()))
             .AddSingleton<ILogger, NuciLogger>()
-            .AddSingleton<IFileRepository<UserEntity>>(x => new XmlRepository<UserEntity>(dataStoreSettings.UserStorePath))
-            .AddSingleton<IFileRepository<SteamAccountEntity>>(x => new XmlRepository<SteamAccountEntity>(dataStoreSettings.SteamAccountStorePath))
-            .AddSingleton<IFileRepository<RewardEntity>>(x => new JsonRepository<RewardEntity>(dataStoreSettings.RewardsStorePath))
+            .AddSingleton<IFileRepository<UserEntity>>(x => new XmlRepository<UserEntity>(x.GetRequiredService<DataStoreSettings>().UserStorePath))
+            .AddSingleton<IFileRepository<SteamAccountEntity>>(x => new XmlRepository<SteamAccountEntity>(x.GetRequiredService<DataStoreSettings>().SteamAccountStorePath))
+            .AddSingleton<IFileRepository<RewardEntity>>(x => new JsonRepository<RewardEntity>(x.GetRequiredService<DataStoreSettings>().RewardsStorePath))
             .AddSingleton<ISteamAccountService, SteamAccountService>()
             .AddSingleton<IRewardService, RewardService>()
             .AddSingleton<IUserService, UserService>();
